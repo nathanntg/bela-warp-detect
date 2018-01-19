2d6d4f3dac4e3cd8b2445cd7e77b7de832473e1d
Nathan Perkins <nathan@maxmo.net>
2018-01-19 11:01:18 -0500

    Matlab wrapper for DTM
    Very basic implementation that allows passing a template and signal. Syntax:
    
    ```
    [scores, lengths] = dtm(template, signal, fs);
    ```

---
+++ BelaWarpDetect/Matlab/compile_mex.m
@@ -0,0 +1,79 @@
+function compile_mex(varargin)
+%COMPILE_MEX Compile MEX interface into the warp detector
+%   No parameters are required, but you can optionally provide parameters:
+%
+%   COMPILE_MEX('debug', true) turns off optimizations, adds debugging
+%   symbols to the binaries and turns on verbose compilation, all to help
+%   debug issues with the MEX files.
+%
+%   COMPILE_MEX('warnings', true) turns on all warnings during compile
+%   time (only tested with Clang compiler).
+
+%% parameters
+debug = false;
+warnings = false;
+
+% load custom parameters
+nparams = length(varargin);
+if 0 < mod(nparams, 2)
+    error('Parameters must be specified as parameter/value pairs');
+end
+for i = 1:2:nparams
+    nm = lower(varargin{i});
+    if ~exist(nm, 'var')
+        error('Invalid parameter: %s.', nm);
+    end
+    eval([nm ' = varargin{i+1};']);
+end
+
+% check platform
+if ~ismac
+    error('Only supports the macOS platform (requires Accelerate framework).');
+end
+
+% print nice message
+fprintf('Compiling functions...\n');
+
+c = {};
+cf = {};
+
+% show warnings
+if warnings
+    cf{end + 1} = '-Weverything';
+end
+
+% enable debugging
+if debug
+    c{end + 1} = '-g';
+    c{end + 1} = '-v';
+else
+    c{end + 1} = '-silent';
+    cf{end + 1} = '-O3';
+end
+
+% add parent directory as include path
+root = fileparts(pwd);
+cf{end + 1} = ['-I' root];
+
+% build cflags
+if numel(cf) > 0
+    c{end + 1} = ['CFLAGS="\$CFLAGS ' strjoin(cf) '"'];
+end
+
+% include Accelerate framework
+if ismac
+    c{end + 1} = 'LDFLAGS="\$LDFLAGS -framework Accelerate"';
+end
+
+% call mex functions
+functions = {{'Matlab/dtm.cpp', 'Library/CircularShortTimeFourierTransform.cpp', 'Library/DynamicTimeMatcher.cpp'}};
+for j = 1:length(functions)
+    if iscell(functions{j})
+        fprintf('%s\n', functions{j}{1});
+        d = [c cellfun(@(x) fullfile(root, x), functions{j}, 'UniformOutput', false)];
+    else
+        fprintf('%s\n', functions{j});
+        d = [c fullfile(root, [functions{j} '.cpp'], 'UniformOutput', false)];
+    end
+    mex(d{:});
+end
---
+++ BelaWarpDetect/Matlab/dtm.cpp
@@ -0,0 +1,181 @@
+//
+//  dtm.cpp
+//  BelaWarpDetect
+//
+//  Created by Nathan Perkins on 1/16/18.
+//  Copyright © 2018 Nathan Perkins. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <mex.h>
+
+#include "Library/CircularShortTimeFourierTransform.hpp"
+#include "Library/DynamicTimeMatcher.hpp"
+
+#define MX_TEST(TEST, ERR_ID, ERR_STR) if (!(TEST)) { mexErrMsgIdAndTxt(ERR_ID, ERR_STR); }
+
+static bool testScalar(const mxArray *in) {
+    // type
+    if (!mxIsDouble(in) && !mxIsSingle(in)) {
+        return false;
+    }
+    
+    // real
+    if (mxIsComplex(in)) {
+        return false;
+    }
+    
+    // dimensions
+    if (mxGetNumberOfDimensions(in) != 2 || mxGetM(in) != 1 || mxGetN(in) != 1) {
+        return false;
+    }
+    
+    return true;
+}
+
+static double getScalar(const mxArray *in, const char *err_id, const char *err_str) {
+    /* check scalar */
+    if (!testScalar(in)) {
+        mexErrMsgIdAndTxt(err_id, err_str);
+    }
+    
+    /* get the scalar input */
+    return mxGetScalar(in);
+}
+
+static bool testVector(const mxArray *in) {
+    // type
+    if (!mxIsDouble(in) && !mxIsSingle(in)) {
+        return false;
+    }
+    
+    // real
+    if (mxIsComplex(in)) {
+        return false;
+    }
+    
+    // dimensions
+    if (mxGetNumberOfDimensions(in) != 2) {
+        return false;
+    }
+    
+    // make sure there is at least one singleton dimension
+    if (mxGetM(in) != 1 && mxGetN(in) != 1) {
+        return false;
+    }
+    
+    // make sure there is at least one non-zero dimension
+    if (mxGetM(in) < 2 && mxGetN(in) < 2) {
+        return false;
+    }
+    
+    return true;
+}
+
+template <typename T> void getVector(const mxArray *in, std::vector<T> &vec, const char *err_id, const char *err_str) {
+    // check vector
+    if (!testVector(in)) {
+        mexErrMsgIdAndTxt(err_id, err_str);
+    }
+    
+    // allocate pointer
+    T *values;
+    size_t sn, sm, sl;
+    
+    // get dimensions
+    sn = mxGetN(in);
+    sm = mxGetM(in);
+    sl = sn > 1 ? sn : sm;
+    
+    // resize vector
+    vec.resize(sl);
+    
+    if (mxIsDouble(in)) {
+        double *values = (double *)mxGetPr(in);
+        
+        // fill vector
+        for (size_t i = 0; i < sl; ++i) {
+            vec[i] = (T)values[i];
+        }
+    }
+    else if (mxIsSingle(in)) {
+        float *values = (float *)mxGetPr(in);
+        
+        // fill vector
+        for (size_t i = 0; i < sl; ++i) {
+            vec[i] = (T)values[i];
+        }
+    }
+}
+
+/* the gateway function */
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+    std::vector<float> signal, templ;
+    float sampling_rate;
+    
+    /* check for proper number of arguments */
+    MX_TEST(nrhs == 3, "MATLAB:dtm:invalidNumInputs", "Requires three inputs (template, signal, sampling rate).");
+    MX_TEST(nlhs == 2, "MATLAB:dtm:invalidNumOutputs", "Requires two outputs (scores and lengths).");
+    
+    /* read arguments */
+    getVector(prhs[0], templ, "MATLAB:dtm:invalidInput", "The template must be a real vector.");
+    getVector(prhs[1], signal, "MATLAB:dtm:invalidInput", "The signal must be a real vector.");
+    sampling_rate = (float)getScalar(prhs[2], "MATLAB:dtm:invalidInput", "The sampling rate must be a real scalar.");
+    
+    /* parameters */
+    unsigned int stft_len = 512, stft_stride = 40;
+    double freq_lo = 1e3, freq_hi = 9e3;
+    
+    /* create STFT */
+    CircularShortTermFourierTransform stft;
+    stft.Initialize(stft_len, stft_stride, signal.size() + stft_len);
+    unsigned int idx_lo = stft.ConvertFrequencyToIndex(freq_lo, sampling_rate);
+    unsigned int idx_hi = stft.ConvertFrequencyToIndex(freq_hi, sampling_rate);
+    
+    /* allocate space for outputs */
+    unsigned int len_feature = stft.GetLengthPower();
+    unsigned int len_templ = stft.ConvertSamplesToColumns(templ.size());
+    unsigned int len_signal = stft.ConvertSamplesToColumns(signal.size());
+    
+    /* generate template */
+    stft.WriteValues(templ);
+    std::vector<float> col;
+    std::vector<std::vector<float>> features_templ;
+    for (unsigned int i = 0; i < len_templ; ++i) {
+        if (!stft.ReadPower(col)) {
+            mexErrMsgIdAndTxt("MATLAB:dtm:internalError", "Unable to generate expected number of spectral columns for the template.");
+        }
+        
+        // slice, slow
+        features_templ.push_back(std::vector<float>(col.begin() + idx_lo, col.begin() + idx_hi));
+    }
+    
+    /* make matcher */
+    DynamicTimeMatcher dtm;
+    dtm.Initialize(features_templ);
+    
+    /* generate outputs */
+    double *scores;
+    double *lengths;
+    plhs[0] = mxCreateDoubleMatrix(1, len_signal, mxREAL);
+    scores = mxGetPr(plhs[0]);
+    plhs[1] = mxCreateDoubleMatrix(1, len_signal, mxREAL);
+    lengths = mxGetPr(plhs[1]);
+    
+    /* process signal */
+    stft.Clear();
+    stft.WriteValues(signal);
+    for (unsigned int i = 0; i < len_signal; ++i) {
+        if (!stft.ReadPower(col)) {
+            mexErrMsgIdAndTxt("MATLAB:dtm:internalError", "Unable to generate expected number of spectral columns for the signal.");
+        }
+        
+        // result
+        auto result = dtm.IngestFeatureVector(std::vector<float>(col.begin() + idx_lo, col.begin() + idx_hi));
+        
+        // append to results
+        scores[i] = (double)result.score;
+        lengths[i] = (double)result.len_diff;
+    }
+}
