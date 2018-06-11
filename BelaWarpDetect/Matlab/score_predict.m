0ad6d2afb42f8522be7be11b2468bb7c90b9ce12
Nathan Perkins <nathan@maxmo.net>
2018-06-11 10:02:19 -0400

    Rewrite scoring funciton
    Better evaluation of TP / TN, basing results off the actual timepoints.

--- BelaWarpDetect/Matlab/score_predict.m
+++ BelaWarpDetect/Matlab/score_predict.m
@@ -1,39 +1,45 @@
 function confusion = score_predict(actual, predict)
 %SCORE_PREDICT Summary of this function goes here
 
+% no predictions? all actual entries are false negatives
 if isempty(predict)
     confusion = [0, 0; length(actual), 0];
     return;
 end
 
-if isempty(actual)
-    confusion = [0, length(predict); 0, 0];
-    return;
-end
-
 % debounce predict
 if length(actual) > 1
-    tm = min(0.05, min(diff(actual)) / 2);
+    tm = min(diff(actual)) / 2;
+    tm = max(min(tm, 0.1), 0.01); % bound to reasonable values
 else
     tm = 0.05;
 end
-debounce = predict(1:(end-1)) + tm;
-to_debounce = predict(2:end) < debounce;
-to_debounce = [false; to_debounce(:)];
-predict(to_debounce) = [];
 
-% match actual and predicted
-df = abs(bsxfun(@minus, actual(:), predict(:)'));
+% no actual? nothing to detect
+if isempty(actual)
+    tp = 0;
+    fn = 0;
+else
+    % match actual and predicted
+    df = abs(bsxfun(@minus, actual(:), predict(:)'));
 
-a = min(df, [], 1);
-b = min(df, [], 2);
+    a = min(df, [], 1);
+    b = min(df, [], 2);
 
-tp = sum(a < 0.05);
-fp = length(predict) - tp;
-fn = sum(b >= 0.05);
+    tp = sum(b < tm);
+    fn = length(actual) - tp;
+end
+
+% poor predictions (debounce poor predictions (only count once per tm,
+% basically)
+poor_predict = predict(a > tm);
+if isempty(poor_predict)
+    fp = 0;
+else
+    fp = 1 + sum(diff(poor_predict) > tm);
+end
 
 confusion = [0, fp; fn, tp];
     
-
 end
 
